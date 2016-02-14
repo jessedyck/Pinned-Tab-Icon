6aa310921183570d6d147b5538f50591f4c64b68
Jesse Dyck <jessedyck@me.com>
2016-02-13 21:17:29 -0600

    Changed Function/Option Namespacing
    Changed to be less generic so I can know what belongs to me during
    uninstall.

--- pinned-tab-icon.php
+++ pinned-tab-icon.php
@@ -2,9 +2,9 @@
 /*
 Plugin Name: Pinned Tab Icon
 Plugin URI: https://github.com/jessedyck/Pinned-Tab-Icon
-Description: A WordPress Plugin that adds Customizer controls to select an SVG image file and set the colour, for use in Safari 9's Pinned Tab feature. See https://support.apple.com/kb/PH21462?locale=en_US for details from Apple.
+Description: A WordPress Plugin that adds Customizer controls (in the Site Icon section) to select an SVG image file and set the colour, for use in Safari 9's Pinned Tab feature. See https://support.apple.com/kb/PH21462?locale=en_US for details from Apple.
 Author: Jesse Dyck
-Version: 1.02
+Version: 1.1
 Author URI: http://jessedyck.me
 */
 
@@ -12,23 +12,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-
 // Register customizer controls
-function jd_mask_icon_customizer ($wp_customize)
+function jd_pinned_icon_customizer ($wp_customize)
 {
 	// Image field
-	$wp_customize->add_setting( 'mask_icon_image', array(
+	$wp_customize->add_setting( 'jd_pinned_icon_image', array(
 	    'default'        => '',
 	    'type'           => 'option',
 	    'sanitize_callback' => 'absint' // required by Theme Checker - also just a good idea
 	) );
 	
 	$wp_customize->add_control( 
-		new WP_Customize_Media_Control( $wp_customize, 'mask_icon_control', array(
+		new WP_Customize_Media_Control( $wp_customize, 'jd_pinned_icon_control', array(
 		    'label'   => 'Pinned Tab Image (SVG)',
 		    'description' => 'SVG file with black shapes with transparent backgrounds only. This is used  to represent your site in Safari 9\'s Pinned Tabs feature. For details, see <a href="https://support.apple.com/kb/PH21462?locale=en_US" title="Safari\'s Pinned Tabs">support.apple.com</a>',
 		    'section' => 'title_tagline',
-		    'settings'   => 'mask_icon_image',
+		    'settings'   => 'jd_pinned_icon_image',
 		    'mime_type' => 'image/svg+xml',
 		    'priority' => 80
 		) 
@@ -36,23 +35,23 @@ function jd_mask_icon_customizer ($wp_customize)
 	
 	
 	// Colour field
-	$wp_customize->add_setting( 'mask_icon_colour', array(
+	$wp_customize->add_setting( 'jd_pinned_icon_colour', array(
 	    'default'        => '#000000',
 	    'type'           => 'option',
 	    'sanitize_callback' => 'sanitize_hex_color' // required by Theme Checker - also just a good idea
 	) );
 	
 	$wp_customize->add_control(
-		new WP_Customize_Color_Control( $wp_customize, 'mask_icon_color_control', array(
+		new WP_Customize_Color_Control( $wp_customize, 'jd_pinned_icon_color_control', array(
 		  'label' => 'Pinned Tab Image Color',
 		  'description' => 'Define the colour for the image',
 		  'section' => 'title_tagline',
-		  'settings' => 'mask_icon_colour',
+		  'settings' => 'jd_pinned_icon_colour',
 		  'priority' => 100
 		) 
 	) );
 }
-add_action( 'customize_register', 'jd_mask_icon_customizer' );
+add_action( 'customize_register', 'jd_pinned_icon_customizer' );
 
 
 // Add SVG support if not already existing
@@ -70,9 +69,9 @@ add_filter('upload_mimes', 'jd_custom_upload_mimes');
 
 
 // Action to output the mask icon HTML tag
-function jd_mask_icon ()
+function jd_pinned_icon_output ()
 {
-	$img_id = get_option( 'mask_icon_image' );
+	$img_id = get_option( 'jd_pinned_icon_image' );
 	
 	// If the ID is invalid, don't output a link; bail
 	if (!($img_id > -1))
@@ -80,7 +79,7 @@ function jd_mask_icon ()
 		
 	$img_url = wp_get_attachment_url ( $img_id );
 		
-	$colour = get_option ( 'mask_icon_colour' );
+	$colour = get_option ( 'jd_pinned_icon_colour' );
 	
 	// Check again that we have a valid hex code
 	$colour = ( preg_match ("|^#([A-Fa-f0-9]{3}){1,2}$|", $colour ) ? $colour : '#000000' );
@@ -89,7 +88,7 @@ function jd_mask_icon ()
 		echo '<link rel="mask-icon" href="'. $img_url . '" color="' . $colour . '">';
 
 }
-add_action ( 'wp_head', 'jd_mask_icon' );
+add_action ( 'wp_head', 'jd_pinned_icon_output' );
 
 
 function jd_is_svg ($id)
